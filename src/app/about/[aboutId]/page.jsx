8203bf215ebaf0036090f92af111cf15612ad592
tikrack <tikrackcode@gmail.com>
2025-03-29 22:02:35 +0330

    redirect - resirect

--- src/app/about/[aboutId]/page.jsx
+++ src/app/about/[aboutId]/page.jsx
@@ -1,11 +1,16 @@
-import Link from "next/link";
+"use client"
 
-const Page = ({ params }) => (
-    <>
-        { params.aboutId }
-        <br/>
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+
+const Page = ({ params }) => {
+    const router = useRouter();
+
+    return (<>
         <Link href={"/"}>go back</Link>
-    </>
-)
+        <br/>
+        <button onClick={() => router.push("/")}>---redirect---</button>
+    </>)
+}
 
 export default Page
